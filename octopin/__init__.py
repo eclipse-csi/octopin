4095993de94f9b2be1c7275a580e9b20dfec0819
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-12-05 14:53:41 +0100

    initial import

---
+++ octopin/__init__.py
@@ -0,0 +1,10 @@
+#  *******************************************************************************
+#  Copyright (c) 2023-2024 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
+
+__appname__ = "octopin"
+__version__ = "0.1.0.dev0"
